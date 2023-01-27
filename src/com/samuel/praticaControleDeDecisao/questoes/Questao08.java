be196576536607db66e342ea43876f49d26940cc
Samuel Machado Duarte <samuelmachado41@gmail.com>
2023-01-27 16:20:34 -0300

    Questao 8 - o menor dentre 3

--- src/com/samuel/praticaControleDeDecisao/questoes/Questao08.java
+++ src/com/samuel/praticaControleDeDecisao/questoes/Questao08.java
@@ -9,7 +9,18 @@ public class Questao08 {
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner (System.in);
-
+		System.out.println("Digite 3 preços de produtos, separando pressionando Enter: ");
+		double numero1 = scan.nextDouble();
+		double numero2 = scan.nextDouble();
+		double numero3 = scan.nextDouble();
+		
+		if(numero1 < numero2 && numero1 < numero3) {
+			System.out.println("Voce deve comprar o produto numero 1 de R$" + numero1);
+		}else if(numero2 < numero1 && numero2 < numero3) {
+			System.out.println("Voce deve comprar o produto numero 2 de R$" + numero2);
+		}else {
+			System.out.println("Voce deve comprar o produto numero 3 de R$" + numero3);
+		}
 	}
 
 }
