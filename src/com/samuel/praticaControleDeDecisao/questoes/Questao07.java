30c7745b1eff88acabd108db6e2a8d39a4b4b290
Samuel Machado Duarte <samuelmachado41@gmail.com>
2023-01-27 16:18:03 -0300

    Questao 7 - o maior e menor dentre 3

--- src/com/samuel/praticaControleDeDecisao/questoes/Questao07.java
+++ src/com/samuel/praticaControleDeDecisao/questoes/Questao07.java
@@ -8,7 +8,36 @@ public class Questao07 {
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner (System.in);
-
+		System.out.println("Digite 3 numeros, separando pressionando Enter: ");
+		double numero1 = scan.nextDouble();
+		double numero2 = scan.nextDouble();
+		double numero3 = scan.nextDouble();
+		
+		if(numero1 > numero2 && numero1 > numero3) {
+			if(numero2 > numero3) {
+				System.out.println("O maior é " + numero1);
+				System.out.println("O menor é " + numero3);
+			}else {
+				System.out.println("O maior é " + numero1);
+				System.out.println("O menor é " + numero2);
+			}
+		}else if(numero2 > numero1 && numero2 > numero3) {
+			if(numero1 > numero3) {
+				System.out.println("O maior é " + numero2);
+				System.out.println("O menor é " + numero3);
+			}else {
+				System.out.println("O maior é " + numero2);
+				System.out.println("O menor é " + numero1);
+			}
+		}else {
+			if(numero1 > numero2) {
+				System.out.println("O maior é " + numero3);
+				System.out.println("O menor é " + numero2);
+			}else {
+				System.out.println("O maior é " + numero3);
+				System.out.println("O menor é " + numero1);
+			}
+		}
 	}
 
 }
