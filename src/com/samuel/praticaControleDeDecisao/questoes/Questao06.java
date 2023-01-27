74fddcb7a07ebf63e9e081f2713fb14c4e87340a
Samuel Machado Duarte <samuelmachado41@gmail.com>
2023-01-27 16:14:48 -0300

    Questão 6 - o maior entre 3

--- src/com/samuel/praticaControleDeDecisao/questoes/Questao06.java
+++ src/com/samuel/praticaControleDeDecisao/questoes/Questao06.java
@@ -8,6 +8,18 @@ public class Questao06 {
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner (System.in);
+		System.out.println("Digite 3 numeros, separando pressionando Enter: ");
+		double numero1 = scan.nextDouble();
+		double numero2 = scan.nextDouble();
+		double numero3 = scan.nextDouble();
+		
+		if(numero1 > numero2 && numero1 > numero3) {
+			System.out.println("O maior é " + numero1);
+		}else if(numero2 > numero1 && numero2 > numero3) {
+			System.out.println("O maior é " + numero2);
+		}else {
+			System.out.println("O maior é " + numero3);
+		}
 
 	}
 
