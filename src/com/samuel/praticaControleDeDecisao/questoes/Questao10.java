dd3fdc00e989ecfecf8f0411f8ee6d945b339a94
Samuel Machado Duarte <samuelmachado41@gmail.com>
2023-01-27 16:28:13 -0300

    Questao 10 - Turnos

--- src/com/samuel/praticaControleDeDecisao/questoes/Questao10.java
+++ src/com/samuel/praticaControleDeDecisao/questoes/Questao10.java
@@ -10,7 +10,17 @@ public class Questao10 {
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner (System.in);
-
+		System.out.println("Em que turno voce estuda?( M - Matutino, V - Vespertino, N - Noturno )");
+		String turno = scan.next();
+		if(turno.equals("M")) {
+			System.out.println("Bom dia!");
+		}else if(turno.equals("V")) {
+			System.out.println("Boa tarde!");
+		}else if(turno.equals("N")) {
+			System.out.println("Boa noite");
+		}else {
+			System.out.println("Valor invalido!");
+		}
 	}
 
 }
