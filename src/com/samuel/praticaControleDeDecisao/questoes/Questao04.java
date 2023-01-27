87b150e7dd81f5768e00d9bbe3bcdfe78a05c18f
Samuel Machado Duarte <samuelmachado41@gmail.com>
2023-01-27 15:53:04 -0300

    Questao 4 - Vogal ou Consoante

--- src/com/samuel/praticaControleDeDecisao/questoes/Questao04.java
+++ src/com/samuel/praticaControleDeDecisao/questoes/Questao04.java
@@ -2,11 +2,19 @@ package com.samuel.praticaControleDeDecisao.questoes;
 
 import java.util.Scanner;
 
+/* 4. Faça um Programa que verifique se uma letra digitada é vogal ou 
+consoante */
 public class Questao04 {
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner (System.in);
-
+		String letra = scan.next();
+		if (letra.equals("a") || letra.equals("e") || letra.equals("i") || letra.equals("o") || letra.equals("u"))
+		{
+			System.out.println("Vogal");
+		}else {
+			System.out.println("Consoante");
+		}
 	}
 
 }
