eff8d331f94692597ce633ecde6ac89478097b4b
Samuel Machado Duarte <samuelmachado41@gmail.com>
2023-01-27 16:23:28 -0300

    questao 9 - ordem decrescente

--- src/com/samuel/praticaControleDeDecisao/questoes/Questao09.java
+++ src/com/samuel/praticaControleDeDecisao/questoes/Questao09.java
@@ -8,7 +8,42 @@ public class Questao09 {
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner (System.in);
-
+		System.out.println("Digite 3 numeros, separando pressionando Enter: ");
+		double numero1 = scan.nextDouble();
+		double numero2 = scan.nextDouble();
+		double numero3 = scan.nextDouble();
+		
+		if(numero1 > numero2 && numero1 > numero3) {
+			if(numero2 > numero3) {
+				System.out.println(numero1);
+				System.out.println(numero2);
+				System.out.println(numero3);
+			}else {
+				System.out.println(numero1);
+				System.out.println(numero3);
+				System.out.println(numero2);
+			}
+		}else if(numero2 > numero1 && numero2 > numero3) {
+			if(numero1 > numero3) {
+				System.out.println(numero2);
+				System.out.println(numero1);
+				System.out.println(numero3);
+			}else {
+				System.out.println(numero2);
+				System.out.println(numero3);
+				System.out.println(numero1);
+			}
+		}else {
+			if(numero1 > numero2) {
+				System.out.println(numero3);
+				System.out.println(numero1);
+				System.out.println(numero2);
+			}else {
+				System.out.println(numero3);
+				System.out.println(numero2);
+				System.out.println(numero1);
+			}
+		}
 	}
 
 }
