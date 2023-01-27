2c77e74f3a520960cee0b2efa595195d347a6e52
Samuel Machado Duarte <samuelmachado41@gmail.com>
2023-01-27 15:43:37 -0300

    Questao 2 - valor positivo/negativo
    2. Faça um Programa que peça um valor e mostre na tela se o valor é
    positivo ou negativo

--- src/com/samuel/praticaControleDeDecisao/questoes/Questao02.java
+++ src/com/samuel/praticaControleDeDecisao/questoes/Questao02.java
@@ -2,12 +2,21 @@ package com.samuel.praticaControleDeDecisao.questoes;
 
 import java.util.Scanner;
 
+/* 2. Faça um Programa que peça um valor e mostre na tela se o valor é 
+positivo ou negativo */
 public class Questao02 {
 
 	public static void main(String[] args) {
 
 		Scanner scan = new Scanner (System.in);
 		
+		System.out.println("Digite um valor : ");
+		double valor = scan.nextDouble();
+		if ( valor >= 0 ) {
+			System.out.println("Valor positivo!");
+		} else {
+			System.out.println("Valor negativo!");
+		}
 	}
 
 }
