75bd1a96f10c00c8895745b0b3ee6f83500bd717
Samuel Machado Duarte <samuelmachado41@gmail.com>
2023-01-27 15:47:15 -0300

    Questao 3 -  F - M - Invalido

--- src/com/samuel/praticaControleDeDecisao/questoes/Questao03.java
+++ src/com/samuel/praticaControleDeDecisao/questoes/Questao03.java
@@ -2,11 +2,21 @@ package com.samuel.praticaControleDeDecisao.questoes;
 
 import java.util.Scanner;
 
+/* 3. Faça um Programa que verifique se uma letra digitada é "F" ou "M". 
+Conforme a letra escrever: F - Feminino, M - Masculino, Sexo Inválido. */
 public class Questao03 {
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner (System.in);
 
+		String letra = scan.next();
+		if(letra.equals("F") || letra.equals("f")) {
+			System.out.println("F - Feminino");
+		}else if(letra.equals("M") || letra.equals("m")) {
+			System.out.println("M - Masculino");
+		}else {
+			System.out.println("Sexo inválido");
+		}
 	}
 
 }
