b45656cb6496c6b13abfc654c8ea40eeae835ac2
Samuel Machado Duarte <samuelmachado41@gmail.com>
2023-01-27 15:35:50 -0300

    Questao1 - Imprimir maior numero
    1. Faça um Programa que peça dois números e imprima o maior deles.

--- src/com/samuel/praticaControleDeDecisao/questoes/Questao01.java
+++ src/com/samuel/praticaControleDeDecisao/questoes/Questao01.java
@@ -1,9 +1,25 @@
 package com.samuel.praticaControleDeDecisao.questoes;
 
+import java.util.Scanner;
+
+//1. Faça um Programa que peça dois números e imprima o maior deles.
 public class Questao01 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Scanner scan = new Scanner (System.in);
+		
+		System.out.println("Digite um numero: ");
+		int numero1 = scan.nextInt();
+		
+		System.out.println("Digite outro numero: ");
+		int numero2 = scan.nextInt();
+		
+		if(numero1 > numero2) {
+			System.out.println(numero1);
+		}else {
+			System.out.println(numero2);
+		}
 
 	}
 
