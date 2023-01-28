511d5ee278ae333986ee293e64d306db89e7bf9d
Samuel Machado Duarte <samuelmachado41@gmail.com>
2023-01-28 13:49:24 -0300

    Questao 12 - impostos

--- src/com/samuel/praticaControleDeDecisao/questoes/Questao12.java
+++ src/com/samuel/praticaControleDeDecisao/questoes/Questao12.java
@@ -9,24 +9,67 @@ FGTS corresponde a 11% do Salário Bruto, mas não é descontado (é
 a empresa que deposita). O Salário Líquido corresponde ao Salário 
 Bruto menos os descontos. O programa deverá pedir ao usuário o 
 valor da sua hora e a quantidade de horas trabalhadas no mês.
+
 o Desconto do IR:
 o Salário Bruto até 900 (inclusive) - isento
 o Salário Bruto até 1500 (inclusive) - desconto de 5%
 o Salário Bruto até 2500 (inclusive) - desconto de 10%
 o Salário Bruto acima de 2500 - desconto de 20% Imprima na tela 
-as informações, dispostas conforme o exemplo abaixo. No 
-exemplo o valor da hora é 5 e a quantidade de hora é 220.
+as informações, dispostas conforme o exemplo abaixo:
+
+No exemplo o valor da hora é 5 e a quantidade de hora é 220.
 Salário Bruto: (5 * 220) : R$ 1100,00 
 (-) IR (5%) : R$ 55,00 
 (-) INSS ( 10%) : R$ 110,00 
 FGTS (11%) : R$ 121,00 
 Total de descontos : R$ 165,00 
-Salário Liquido : R$ 935,00*/
+Salário Liquido : R$ 935,00
+*/
 public class Questao12 {
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner (System.in);
-
+		double salarioLiquido = 0;
+		double salarioBruto = 0;
+		double valorHora;
+		double irPorcentagem;
+		double ir;
+		double inss = 10;
+		double fgts = 11;
+		double sindicato = 3;
+		double totalDesconto;
+		
+		System.out.println("Digite o valor da sua hora: ");
+		valorHora = scan.nextDouble();
+		System.out.println("Digite quando horas vc trabalha no mes: ");
+		double horasTrabalhadas = scan.nextDouble();
+		
+		salarioBruto = valorHora * horasTrabalhadas;
+		
+		if(salarioBruto <= 900) {
+			irPorcentagem = 0;
+		}else if(salarioBruto <= 1500) {
+			irPorcentagem = 5;
+		}else if(salarioBruto <= 2500) {
+			irPorcentagem = 10;
+		}else {
+			irPorcentagem = 20;
+		}
+		
+		ir = salarioBruto * (irPorcentagem/100);
+		inss = salarioBruto * (inss/100);
+		fgts = salarioBruto * (fgts/100);
+		sindicato = salarioBruto * (sindicato/100);
+		totalDesconto = ir + inss + sindicato;
+		salarioLiquido = salarioBruto - totalDesconto;
+		
+		System.out.println("Salario Bruto ( " + valorHora + " * " + horasTrabalhadas + " ) : R$" + salarioBruto);
+		System.out.println("(-) IR ( " + irPorcentagem + "% ) : R$" + ir);
+		System.out.println("(-) INSS ( 10% ) : R$" + inss);
+		System.out.println("(-) Sindicato ( 3% ) : R$" + sindicato);
+		System.out.println("Total de descontos : R$" + totalDesconto);
+		System.out.println("Salario Liquido : R$" + salarioLiquido);
+		
 	}
 
 }
