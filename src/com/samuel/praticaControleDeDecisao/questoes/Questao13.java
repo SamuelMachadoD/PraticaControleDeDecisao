5dbccd65a4a5a364fca1098c442b89a7c693e059
Samuel Machado Duarte <samuelmachado41@gmail.com>
2023-01-28 14:14:25 -0300

    Questao 13 - switch

--- src/com/samuel/praticaControleDeDecisao/questoes/Questao13.java
+++ src/com/samuel/praticaControleDeDecisao/questoes/Questao13.java
@@ -9,6 +9,34 @@ public class Questao13 {
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner (System.in);
+		System.out.println("Digite um numero de 1 a 7");
+		int dia = scan.nextInt();
+		switch(dia) {
+		case 1:
+			System.out.println("Domingo");
+			break;
+		case 2:
+			System.out.println("Segunda-Feira");
+			break;
+		case 3:
+			System.out.println("Terça-feira");
+			break;
+		case 4:
+			System.out.println("Quarta-Feira");
+			break;
+		case 5:
+			System.out.println("Quinta-Feira");
+			break;
+		case 6:
+			System.out.println("Sexta-Feira");
+			break;
+		case 7:
+			System.out.println("Sabado");
+			break;
+		default:
+			System.out.println("Valor invalido");
+			break;
+		}
 
 	}
 
