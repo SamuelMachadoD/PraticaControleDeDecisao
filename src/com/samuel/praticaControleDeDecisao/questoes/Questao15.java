f605ae54709c605bb7fe526461ce6429eff84e21
Samuel Machado Duarte <samuelmachado41@gmail.com>
2023-01-28 15:25:30 -0300

    Questao 15 - triangulos

--- src/com/samuel/praticaControleDeDecisao/questoes/Questao15.java
+++ src/com/samuel/praticaControleDeDecisao/questoes/Questao15.java
@@ -16,7 +16,25 @@ public class Questao15 {
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner (System.in);
+		System.out.println("Lado 1 do triangulo: ");
+		double lado1 = scan.nextDouble();
+		System.out.println("Lado 2 do triangulo: ");
+		double lado2 = scan.nextDouble();
+		System.out.println("Lado 3 do triangulo: ");
+		double lado3 = scan.nextDouble();
 		
+		if(lado1 + lado2 >= lado3 || lado3 + lado2 >= lado1 || lado3 + lado1 >= lado2){
+			System.out.println("Triangulo");
+			if(lado1 == lado2 && lado1 == lado3) {
+				System.out.println("Equilatero");
+			}else if(lado1 == lado2 || lado1 == lado3 || lado2 == lado3) {
+				System.out.println("Isoceles");
+			}else {
+				System.out.println("Escaleno");
+			}
+		}else {
+			System.out.println("Nao e um triangulo");
+		}
 
 	}
 
