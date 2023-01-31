85afeda592b3acf616a9d8c1c7121ced2f6fe240
Samuel Machado Duarte <samuelmachado41@gmail.com>
2023-01-31 15:25:45 -0300

    Questao 17 - Ano bissexto

--- src/com/samuel/praticaControleDeDecisao/questoes/Questao17.java
+++ src/com/samuel/praticaControleDeDecisao/questoes/Questao17.java
@@ -7,8 +7,20 @@ determinado ano e em seguida informe se este ano é ou não bissexto.*/
 public class Questao17 {
 
 	public static void main(String[] args) {
+		int ano = 0;
+		
 		Scanner scan = new Scanner (System.in);
-
+		
+		System.out.println("Digite o ano: ");
+		ano = scan.nextInt();
+		
+        if(ano % 400 == 0){
+            System.out.println(ano + " é bissexto.");
+        } else if((ano % 4 == 0) && (ano % 100 != 0)){
+            System.out.println(ano + " é bissexto.");
+        } else{
+            System.out.println(ano + " não é bissexto");
+        }
 	}
 
 }
