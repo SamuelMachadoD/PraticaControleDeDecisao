4e6f5834aef6a89b3c99b64d60bb64db5fbb7c90
Samuel Machado Duarte <samuelmachado41@gmail.com>
2023-01-31 15:10:03 -0300

    Questão 16 - Calculo de raiz de 2 grau

--- src/com/samuel/praticaControleDeDecisao/questoes/Questao16.java
+++ src/com/samuel/praticaControleDeDecisao/questoes/Questao16.java
@@ -18,7 +18,40 @@ informe-as ao usuário; */
 public class Questao16 {
 
 	public static void main(String[] args) {
+		double a, b, c, delta, baskhara = 0;
+		
 		Scanner scan = new Scanner (System.in);
+		
+		System.out.println("Digite o valor de a: ");
+		a = scan.nextDouble();
+		
+		if(a != 0) {
+			System.out.println("Digite o valor de b: ");
+			b = scan.nextDouble();
+			System.out.println("Digite o valor de c: ");
+			c = scan.nextDouble();
+			
+			delta = Math.pow(b, 2) - 4 * a * c;
+			
+			
+			if(delta < 0) {
+				System.out.println("A equacao nao possui raizes reais! Programa encerrado!");
+			}else if(delta == 0) {
+				System.out.println("A equacao possui apenas 1 raiz real!");
+				baskhara = (-b + Math.sqrt(delta))/2*a;
+				System.out.println(baskhara);
+			}else {
+				System.out.println("A equacao possui 2 raizes reais!");
+				baskhara = (-b + Math.sqrt(delta))/2*a;
+				System.out.println("Bhaskara postivo " + baskhara);
+				baskhara = (-b - Math.sqrt(delta))/2*a;
+				System.out.println("Bhaskara negativo " + baskhara);
+			}
+			
+		}else {
+			System.out.println("Nao e uma equacao de segundo grau! Programa encerrado!");
+		}
+		
 	}
 
 }
