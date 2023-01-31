2355d4a32bf20a61e12876bbac42c3ad31a29472
Samuel Machado Duarte <samuelmachado41@gmail.com>
2023-01-31 16:17:12 -0300

    Questao 5 - indentação

--- src/com/samuel/praticaControleDeDecisao/questoes/Questao05.java
+++ src/com/samuel/praticaControleDeDecisao/questoes/Questao05.java
@@ -13,12 +13,17 @@ import java.util.Scanner;
 public class Questao05 {
 
 	public static void main(String[] args) {
+		
 		Scanner scan = new Scanner (System.in);
+		
 		System.out.println("Digite uma nota parcial: ");
 		double nota1 = scan.nextDouble();
+		
 		System.out.println("Digite a segunda nota: ");
 		double nota2 = scan.nextDouble();
+		
 		double media = (nota1+nota2)/2;
+		
 		if(media >= 10) {
 			System.out.println("Aprovado com Distincao!");
 		}else if(media >= 7) {
