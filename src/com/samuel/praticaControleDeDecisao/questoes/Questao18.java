b20757fb6356b6ffa340597d882af4a35b28383c
Samuel Machado Duarte <samuelmachado41@gmail.com>
2023-01-31 18:02:43 -0300

    Update Questao18.java

--- src/com/samuel/praticaControleDeDecisao/questoes/Questao18.java
+++ src/com/samuel/praticaControleDeDecisao/questoes/Questao18.java
@@ -2,7 +2,7 @@ package com.samuel.praticaControleDeDecisao.questoes;
 
 import java.util.Scanner;
 
-/*22.Faça um Programa que peça um número inteiro e determine se ele é 
+/*18.Faça um Programa que peça um número inteiro e determine se ele é 
 par ou impar. Dica: utilize o operador módulo (resto da divisão). */
 public class Questao18 {
 
