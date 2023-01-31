4c7d7c945a9eabdb57cd9498fff55bca26c917f0
Samuel Machado Duarte <samuelmachado41@gmail.com>
2023-01-31 16:00:26 -0300

    Questao 4 - correção Switch com string

--- src/com/samuel/praticaControleDeDecisao/questoes/Questao04.java
+++ src/com/samuel/praticaControleDeDecisao/questoes/Questao04.java
@@ -9,12 +9,25 @@ public class Questao04 {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner (System.in);
 		String letra = scan.next();
-		if (letra.equals("a") || letra.equals("e") || letra.equals("i") || letra.equals("o") || letra.equals("u"))
+		
+		/** if (letra.equals("a") || letra.equals("e") || letra.equals("i") || letra.equals("o") || letra.equals("u"))
 		{
 			System.out.println("Vogal");
 		}else {
 			System.out.println("Consoante");
+		}*/
+		
+		switch(letra) {
+			case "a":
+			case "e":
+			case "i":
+			case "o":
+			case "u": 
+				System.out.println("Vogal"); 
+				break;
+			default: System.out.println("Consoante");
 		}
+		
 	}
 
 }
